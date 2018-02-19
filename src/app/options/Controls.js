043c7161f16394677a00e0ff8af206babfb786e9
Adrymne <jealousy.of.tomorrow@gmail.com>
2018-02-19 17:53:32 +1100

    Implement Clear Board button

--- src/app/options/Controls.js
+++ src/app/options/Controls.js
@@ -3,16 +3,18 @@ import { Button } from 'reactstrap';
 import FA from '@fortawesome/react-fontawesome';
 import { faPlay, faSyncAlt } from '@fortawesome/fontawesome-free-solid';
 import './Controls.css';
+import { connect } from 'react-redux';
+import { clearBoard } from 'store/actions';
 
-const Controls = () => (
+const Controls = ({ clearBoard }) => (
   <div className="options__controls">
     <Button outline color="success" title="Start game">
       <FA icon={faPlay} />
     </Button>
-    <Button outline color="warning" title="Clear board">
+    <Button outline color="warning" title="Clear board" onClick={clearBoard}>
       <FA icon={faSyncAlt} />
     </Button>
   </div>
 );
 
-export default Controls;
+export default connect(undefined, { clearBoard })(Controls);
